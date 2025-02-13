2d5a0f3bd19486b1b427c866a1fddf9ee728580b
Paulo Turquetti <pauloturquetti29@gmail.com>
2025-02-13 16:04:13 -0300

    Extração de texto mais profundo (sem analizar integridade HTML)

--- HtmlAnalyzer.java
+++ HtmlAnalyzer.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Stack;
 
 
 
@@ -19,9 +20,9 @@ public class HtmlAnalyzer{
 
             URL url = new URL(args[0]); //Getting URL from argvs
             String[] terms = getContent(url);   //Extracting content
-            result = getMaxDepth
+            String result = getMaxDepthText(terms); //Gets the max depth Text
 
-            
+            System.out.println(result);
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -42,17 +43,100 @@ public class HtmlAnalyzer{
             content = content.concat(inLine);
         }
 
+        content = """
+<html>
+
+    <head>
+        <title>
+            Titulo
+        </title>
+    </head>
+
+    <body>
+
+        <div>
+
+            <p>
+                Texto no fundo
+            </p>
+
+        </div>
+
+    </body>
+
+</html>
+""";
+
+
+
+        String[] terms = content.replaceAll("<", " <").replaceAll(">", "> ").replaceAll("\\s+", " ").trim().split(" ");
         
-        String[] terms = content.trim().replaceAll("<", " <").replaceAll(">", "> ").replaceAll("\\s+", " ").split(" ");
-        
-        for(String term:terms){
-            System.out.println(term);
-        }
+        //for(String term:terms){
+        //    System.out.println(term);
+        //}
 
         return terms;
     }
 
+    public static String getMaxDepthText(String[]terms){
 
+        Stack<String> stack = new Stack<>();
+        int depthCount = 0;
+        int maxDepthWithText = 0;
+        String maxDepthText = "";
+        Boolean lastTermWasText = false;
+        Boolean startedConcatMaxDepthText = false;
 
+        System.out.println("Current depth: " + depthCount);
+
+        for(String term:terms){
+            
+            if(term.startsWith("</")){      //Closure tag
+
+                stack.pop();
+                depthCount--;
+                lastTermWasText = false;
+                startedConcatMaxDepthText = false;
+
+                System.out.println("Current depth: " + depthCount);
+                
+
+            }else if(term.startsWith("<")){     //Opening tag
+
+                stack.push(term);
+                depthCount++;
+                lastTermWasText = false;
+                startedConcatMaxDepthText = false;
+
+                System.out.println("Current depth: " + depthCount);
+
+            }else {      //Text
+
+                if(!lastTermWasText){
+
+                    System.out.println(" -New Text found (depth: " + depthCount + ")");
+
+                    if(depthCount > maxDepthWithText){
+                        maxDepthWithText = depthCount;
+                        maxDepthText = term;
+                        startedConcatMaxDepthText = true;
+                    }
+                    
+
+                }else if(startedConcatMaxDepthText){      //Last term was a max Depth text 
+
+                    maxDepthText = maxDepthText.concat(" " + term);
+
+                }
+
+                lastTermWasText = true;
+                
+
+            }
+
+        }
+
+        return maxDepthText;
+    }
 
 }
