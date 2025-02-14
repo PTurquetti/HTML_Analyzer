147343e86325b2d9c4a6c1ee87f42e5b4f9eb2e5
Paulo Turquetti <pauloturquetti29@gmail.com>
2025-02-14 20:07:32 -0300

    Organização do código e comentários

--- HtmlAnalyzer.java
+++ HtmlAnalyzer.java
@@ -6,20 +6,17 @@ import java.util.Stack;
 /*
 TO DO:
 
--Casos especiais de html invalido
--Funções: isTag, isOpeningTag e isClosureTag
--tratamento de excessoes
+-Casos especiais de html mal formado //REAVALIAR
 -Otimizar codigo
 -Analisar questões de segurança do codigo
-- Apagar trechos irrelevantes
 */
 
 public class HtmlAnalyzer{
 
     public static void main(String[] args){
 
-        if(args.length!=1){
-            //missing or too many args
+        if(args.length!=1){     //missing or too many args
+
             System.out.println("Missing or too many args. Try 'java HtmlAnalyzer <URL>'");
             return;
         }  
@@ -29,103 +26,86 @@ public class HtmlAnalyzer{
             String result = getMaxDepthText(getContent(new URL(args[0])));     //Gets the max depth text or HTML malformation
             System.out.println(result);
 
-        } catch (MalformedURLException e) {
-            e.getMessage();
-            System.out.println("URL not valid");
-        } catch (Exception e) {
-            e.getMessage();
-            System.out.println("Error");
+        } catch(MalformedURLException e){   //Malformed URL
+            System.out.println("Malformed URL -" + e.getMessage());
+
+        } catch(IOException e){             //Trouble reading HTML content
+            System.out.println("URL connection error - " + e.getMessage());
+
+        } catch(IllegalStateException e){   //Malformed HTML detected
+            System.out.println("malformed HTML");
+
+        } catch(Exception e){               //Other exceptions
+            System.out.println("Unexpected error: " + e.getMessage());
         }
 
     }
 
     public static String[] getContent(URL url) throws IOException{  //Transform the HTML content into an array of terms
 
-        BufferedReader htmlContent = new BufferedReader(new InputStreamReader(url.openStream()));    //Reding HTML content
-        
-        String line;  
-        String content = "";
+        String content = "";        //Stores the entire HTML content
 
-        
-        while ((line=htmlContent.readLine()) != null){     //Transforming HTML content into a single string
-            content = content.concat(line);
+        try(BufferedReader htmlContent = new BufferedReader(new InputStreamReader(url.openStream()))){  //Reding HTML content from URL
+            
+            String line;  
+            while ((line=htmlContent.readLine()) != null){     //Transforming HTML content into a single string
+                content = content.concat(line);
+            }
+
+        }catch(IOException e){      //Error occured while reading the HTML content
+            throw new IOException("Error while reading html content");
         }
 
-        // v-- APAGAR AQUI QUANDO PRONTO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        content = """
-<html>
-<head>
-<title>
-Este e o titulo.
-</title>
-</head>
-<body>
-<div>
-<p>
-Texto mais profundo
-</p>
-</div>
-</body>
-</html>
-""";
-
-
-
-
-        // transforming content in a vector of terms
-        String[] terms = content.replaceAll("<", " <").replaceAll(">", "> ").replaceAll("\\s+", " ").trim().split(" ");
-        
-        //for(String term:terms){
-        //    System.out.println(term);
-        //}
+        String[] terms = content            //Treating and spliting content into an array of terms
+            .replaceAll("<", " <")          //Adding space before tags
+            .replaceAll(">", "> ")          //Adding space after tags
+            .replaceAll("\\s+", " ")        //Removendo excessive spaces and other space characters
+            .trim()                         //Removing spaces at the beginning and end of the string
+            .split(" ");                    //Splitting content
 
         return terms;
     }
 
-    public static String getMaxDepthText(String[]terms){
+    public static String getMaxDepthText(String[]terms){        //Returns the max depth text or throws IllegalStateException(malformed HTML)
 
-        Stack<String> stack = new Stack<>();             // Stacking tags to control text depths and html malformation
+        Stack<String> stack = new Stack<>();             // Stack used for controlling depth and html malformation
         int maxDepth = 0;                                // Depth of the deepest text
         String maxDepthText = "";                        // maximun depth text
         Boolean lastTermWasMaxDepthText = false;         // Checks if the last term was part of the max depth text
 
-        System.out.println("Current depth: " + stack.size());   //0
-
-        for(String term:terms){      //Checks term by term
+        for(String term:terms){      //Checks HTML content term by term
             
-            if(term.startsWith("</")){      //Term is a closure tag
+            if(isClosureTag(term)){      //Term is a closure tag
 
                 if(!stack.isEmpty() && stack.pop().equals(term.replace("</", "<"))){   //Checks if Stack is not empty and if closing tag matches last opening tag
-                //stack.pop removes stacks's last opening tag
-
-                    lastTermWasMaxDepthText = false;   //Term is no longer part of the maxDepthText
-
-                    System.out.println("Current depth: " + stack.size());
+                    
+                    //stack.pop removes the stacks's last opening tag
+                    lastTermWasMaxDepthText = false;   //Term is not a part of the maxDepthText
 
                 }else{      // Closing tag doesn't match last opening tag or stack had no opening tags in it 
-                    return "malformed HTML";
+                    throw new IllegalStateException("Closure tag doesn't match last opening tag or stack was empty when closure tag was found");
                 }
                 
-            }else if(term.startsWith("<")){     //Term is an opening tag
+            }else if(isOpeningTag(term)){           //Term is an opening tag
 
-                stack.push(term);               //Adds tag to the stack
+                stack.push(term);                   //Adds tag to the stack
+                lastTermWasMaxDepthText = false;    //Term is not a part of the maxDepthText
 
-                lastTermWasMaxDepthText = false;   //Term is no longer part of the maxDepthText
+            }else{                          //Term is a Text
 
-                System.out.println("Current depth: " + stack.size());
+                if(stack.isEmpty()){        //Empty stack indicates tagless text
+                    throw new IllegalStateException("Text found when stack was empty. Text without open tags");
+                }
 
-            }else {                         //Term is a Text
-
-                if(stack.size() > maxDepth){        //This text is the max depth text
-                    maxDepth = stack.size();        //Saves current depth in maxDepth
-                    maxDepthText = term;            //maxDepthTExt becomes the term
+                if(stack.size() > maxDepth){           //This text is the max depth text
+                    
+                    maxDepth = stack.size();           //Saves current depth in maxDepth
+                    maxDepthText = term;               //maxDepthTExt becomes the term
                     lastTermWasMaxDepthText = true;    //If next term is also a text, it is also a part of the max depth text
 
-                    System.out.println(" -New Max Depth Text found (depth: " + stack.size() + ")");
-
                 }else if(lastTermWasMaxDepthText){      //Last term was part of the max Depth text
+                    
                     //This means the current term is also a part of the max depth text
-
                     maxDepthText = maxDepthText.concat(" " + term);     //concatenates the term to the max depth text
 
                 }
@@ -134,23 +114,23 @@ Texto mais profundo
 
         }
 
-        // Special cases:
-        // Primeiro termo não pode ser texto
-        // Ultimo termo tem que ser tag de fechamento
-        // Pilha tem que terminar vazia
-        // não pode haver texto sem tag aberta (verificar se está em tag de texto)
-
-        if(!(terms[0].startsWith("<") && terms[0].endsWith(">"))){ //HTML starts with a text (no opening tags)
-            return "malformed HTML";
-        }
-        if(!(terms[terms.length -1].startsWith("<") && terms[terms.length -1].endsWith(">"))){ //HTML ends with a text (tags opened)
-            return "malformed HTML";
-        }
-        if(!stack.isEmpty()){            //Opening tags were not closed
-            return "malformed HTML";
+        if(!stack.isEmpty()){            //Not all Opening tags were closed when finished reading HTML content
+            throw new IllegalStateException("Stack not empty when finished reading content. Tags were not properly closed");
         }
 
         return maxDepthText;
+    }
+
+    public static Boolean isTag(String term){           //Checks if term is an HTML tag
+        return (term.startsWith("<") && term.endsWith(">"));
+    }
+
+    public static Boolean isClosureTag(String term){    //Checks if term is an HTML closure tag
+        return (isTag(term) && term.startsWith("</"));
+    }
+
+    public static Boolean isOpeningTag(String term){    //Checks if term is an HTML opening tag
+        return (isTag(term) && !isClosureTag(term));
     }
 
 }
